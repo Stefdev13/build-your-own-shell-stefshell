0778db94a4ce6426d29fa27993bde382549dd036
EikcoDev <stef.eikco.bekaert@proton.org>
2026-02-12 22:49:25 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,14 +6,38 @@ const rl = createInterface({
 });
 
 function run() {
-  rl.question("$ ", (command) => {
-    if (command.toLowerCase() == "exit") {
-      process.exit();
-    } else {
-      console.log(`${command}: command not found`);
+  rl.question("$ ", (command: string) => {
+    const indexOfFirstSpace: number = command.indexOf(" ");
+    const firstCommand: string =
+      indexOfFirstSpace > 0
+        ? command.slice(0, indexOfFirstSpace)
+        : command.slice(0);
+
+    const commandParams: string =
+      indexOfFirstSpace > 0 ? command.slice(indexOfFirstSpace).trim() : "";
+
+    switch (firstCommand.toLowerCase()) {
+      case "q":
+        process.exit();
+      case "exit":
+        process.exit();
+      case "echo":
+        rl.write(`${commandParams} \n`);
+        break;
+      default:
+        console.log(`${command}: command not found`);
+        break;
     }
+
     run();
   });
 }
 
 run();
+
+//Run the following to test your shell:
+//  -> bun run ./app/main.ts
+
+//Commands:
+// "codecrafters test" -> test solution
+// "codecrafters submit" -> submit solution
