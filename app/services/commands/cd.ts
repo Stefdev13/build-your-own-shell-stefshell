bc285fe468eadf95b792720877412fcfe9d4f6e1
Stefdev13 <stef.bekaert@proton.me>
2026-02-19 16:06:31 +0100

    codecrafters submit [skip ci]

--- app/services/commands/cd.ts
+++ app/services/commands/cd.ts
@@ -2,14 +2,40 @@ import type { Command } from "../types/command";
 import fs from "fs";
 
 export const cd: Command = (rl, args) => {
-  switch (args[0][0]) {
-    case "/":
-      fs.existsSync(args[0])
-        ? process.chdir(args[0])
-        : rl.write(`cd: ${args}: No such file or directory \n`);
-      break;
-    default:
-      rl.write(`cd: ${args}: No such file or directory \n`);
-      break;
+  try {
+    switch (args[0][0]) {
+      //Absolute paths
+      case "/":
+        fs.existsSync(args[0])
+          ? process.chdir(args[0])
+          : rl.write(`cd: ${args}: No such file or directory \n`);
+        break;
+
+      //Relative paths
+      case ".":
+        let currentDirAsArray: string[];
+        if (process.cwd().includes("/")) {
+          currentDirAsArray = process.cwd().split("/");
+        } else {
+          currentDirAsArray = process.cwd().split("\\");
+        }
+
+        for (const section of args[0].split("/")) {
+          if (section == "..") {
+            currentDirAsArray.pop();
+          } else if (section != ".") {
+            currentDirAsArray.push(section);
+          }
+        }
+
+        process.chdir(currentDirAsArray.join("/"));
+        break;
+
+      default:
+        rl.write(`cd: ${args}: No such file or directory \n`);
+        break;
+    }
+  } catch {
+    rl.write(`cd: ${args}: No such file or directory \n`);
   }
 };
